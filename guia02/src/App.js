e6ac3e74393f6d3e801a9553c264598c9ec143fb
Steven <margueis_46@yahoo.com>
2021-08-13 08:48:34 -0600

    Desarrollo Guia 02

---
+++ guia02/src/App.js
@@ -0,0 +1,17 @@
+import logo from 'react';
+import './App.css';
+import Form from './components/Form';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="App-content">
+        <p>
+          Aqui haremos nuestro TO-DO list
+        </p>
+        <Form />
+      </div>
+    </div>
+  );
+}
+export default App;
